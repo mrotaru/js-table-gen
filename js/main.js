7bc4331834b17d1799c1fe4b7af3b009d43ff16e
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-12-15 22:56:13 +0000

    thead generated

--- js/main.js
+++ js/main.js
@@ -1,105 +1,64 @@
 $( document ).ready(function() {
 
     var data = window.statsCompareData;
-    console.log('data: ', data);
+//    console.log('data: ', data);
 
     var tableGenerator = new TableGenerator();
-    var props = [];
+    var xprops = [];
     for (var i=0; i < data.length; ++i) {
-        props = tableGenerator.extractXProps(data[i],props);
+        xprops = tableGenerator.extractXProps(data[i],xprops);
     }
 
-    console.log('extracted props: ');
-    console.dir(props);
+//    console.log('extracted props: ');
+//    console.dir(xprops);
 
     if(!data.length >= 1) {
         return;
     }
 
-    var $table = $('<table class="table"></table>');
-
-    // number of columns
-    var nrCols = 0;
-
-    function getPropsOnDepth(xprop, depth){
-        if(depth === 0) {
-            return xprops;
-        }
-
-        if((depth > 0 && !xprop.hasOwnProperty('properties')) || depth < 0){
-            return null;
-        }
-
-        for (var i=0; i < xprop.properties.length; ++i) {
-            
-        }
-    }
-
     // build table header
-    // done in two or more passes
-    // 1. build row from top level props. But we need `colspan`
-    var $thead = $('<thead></thead>');
-    function buildHeader(xprops){
-        var recurse = false;
-        var $tr = $('<tr></tr>');
-        for(var i=0; i < props.length; ++i){
-            var prop = xprops[i];
-            $tr.append('<th colspan="' + tableGenerator.getNrOfEdgeProps(prop) + '">' + prop.name + '</th>');
-            if(prop.hasOwnProperty('properties')){
-                recurse = true;
-            }
-            if(recurse){
-            }
-        }
-        $thead.append($tr);
-    }
+    function buildHeader(layeredXProps){
+        var $thead = $('<thead></thead>');
+        for (var i=0; i < layeredXProps.length; ++i) {
 
-        if(typeof(props[i]) === 'string'){
-        } else {
-            var propName = Object.keys(props[i]);
-            $tr.append('<th colspan="' + props[i][propName].length + '">' + propName + '</th>');
+            var $tr = $('<tr></tr>');
+            for(var j=0; j < layeredXProps[i].length; ++j){
+                var prop = layeredXProps[i][j];
+                var colspan = prop.hasOwnProperty('span' ) ? prop.span.toString() : "1";
+                var rowspan = prop.hasOwnProperty('depth') ? prop.depth.toString() : "1";
+                var name = prop.name.charAt(0).toUpperCase() + prop.name.slice(1);
+                var str = '<th colspan="' + colspan + '" rowspan="' + rowspan + '">' + name + '</th>';
+                console.log(str);
+                $tr.append(str);
+            }
+
             $thead.append($tr);
         }
+        return $thead;
     }
 
-    console.log('nrCols: ', nrCols);
+    var layeredXProps = tableGenerator.layerXProps(xprops);
+    var $table = $('<table class="table"></table>');
+    $table.append(buildHeader(layeredXProps));
 
-    // create the second row
-    if(needSecondRow) {
-        var $tr = $('<tr></tr>')
-        _(props).each(function(prop){
-            if(typeof(prop) === 'object') {
-                // sould be an array
-                _(prop[Object.keys(prop)[0]]).each(function(nestedProp){
-                    console.log('nestedProp: ', nestedProp);
-                    $tr.append('<th>' + nestedProp + '</th>');
-                });
-            } else {
-                $tr.append("<td></td>");
-            }
-        });
-        $thead.append($tr);
-    }
-
-    $table.append($thead);
 
     var $tbody = $('<tbody></tbody>');
     // each item
-    _(data).each(function(item){
-        $tr = $('<tr></tr>');
-        // each property
-        _(props).each(function(prop){
-            if(typeof(prop) === 'string'){
-                $tr.append( '<td>' + item[prop] + '</td>' );
-            } else {
-                var mPropName = Object.keys(prop)[0];
-                _(prop[mPropName]).each(function(nestedProp){
-                    $tr.append( '<td>' + item[mPropName][nestedProp] + '</td>' );
-                });
-            }
-        });
-        $tbody.append($tr);
-    });
+//    _(data).each(function(item){
+//        $tr = $('<tr></tr>');
+//        // each property
+//        _(xprops).each(function(prop){
+//            if(typeof(prop) === 'string'){
+//                $tr.append( '<td>' + item[prop] + '</td>' );
+//            } else {
+//                var mPropName = Object.keys(prop)[0];
+//                _(prop[mPropName]).each(function(nestedProp){
+//                    $tr.append( '<td>' + item[mPropName][nestedProp] + '</td>' );
+//                });
+//            }
+//        });
+//        $tbody.append($tr);
+//    });
     $table.append($tbody);
 
     // insert table into dom
